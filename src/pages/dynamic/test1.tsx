b6533144b90a1ea4011d69172538ba770e94d21a
lorne <1991wangliang@gmail.com>
2024-09-16 09:47:20 +0800

    dynamic code to component

--- src/pages/dynamic/test1.tsx
+++ src/pages/dynamic/test1.tsx
@@ -1,15 +1,18 @@
 import React, {Suspense, useState} from "react";
-import {Button, Col, Input, Row, Select, Spin} from "antd";
+import {Button, Col, Input, Row, Select, Space, Spin} from "antd";
 import Editor from "@/components/Editor";
 import {PageContainer} from "@ant-design/pro-components";
 import loadComponent from "@/components/Layout/DynamicCode";
+import {BugFilled} from "@ant-design/icons";
+import {useRoutesContext} from "@/components/Layout/RoutesProvider";
 
 const Test1 = () => {
 
     const [RemoteTestComponent, setRemoteTestComponent] = useState<React.ComponentType<any> | null>(null);
+    const {addMenu} = useRoutesContext();
 
     const defaultCode =
-`
+        `
     const Test = () => {
         return (
             <div>
@@ -27,13 +30,33 @@ const Test1 = () => {
 `;
     const [code, setCode] = useState(defaultCode); // 直接在代码中返回 Test 组件实例
 
-    const executeCode = (codeStr: string) => {
+    const executeCode = (codeStr: string, mode: string) => {
         loadComponent(codeStr, [
             Button,
             Input,
             Select
         ]).then((Component) => {
+
             setRemoteTestComponent(() => Component);
+
+            if (mode === 'menu') {
+                addMenu({
+                    path: '/test',
+                    name: '测试页面',
+                    icon: <BugFilled/>,
+                    routes: [
+                        {
+                            path: '/test/test1',
+                            element: (
+                                <PageContainer>
+                                    <Component/>
+                                </PageContainer>
+                            ),
+                            name: '测试页面1',
+                        }
+                    ]
+                });
+            }
         }).catch((error) => {
             console.error(error);
         });
@@ -44,14 +67,28 @@ const Test1 = () => {
         <PageContainer
             title={"动态加载代码"}
             extra={(
-                <Button
-                    type={"primary"}
-                    onClick={() => {
-                        executeCode(code);
-                    }}
-                >
-                    Run Code
-                </Button>
+                <Space>
+
+                    <Button
+                        type={"primary"}
+                        onClick={() => {
+                            executeCode(code, 'load');
+                        }}
+                    >
+                        Run Code
+                    </Button>
+
+                    <Button
+                        onClick={() => {
+                            executeCode(code, 'menu');
+                        }}
+                    >
+                        Add Menu
+                    </Button>
+
+
+                </Space>
+
             )}
         >
             <Row>
